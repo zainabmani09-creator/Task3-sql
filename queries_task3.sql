357d27a12e42f287c1748de109effd8090f2c5fe
zainabmani09-creator <zainabmani09@gmail.com>
2026-01-19 18:55:49 +0530

    Update queries_task3.sql

--- queries_task3.sql
+++ queries_task3.sql
@@ -1,6 +1,1 @@
-CREATE DATABASE task3_sql;
-USE task3_sql;
-SELECT COUNT(*) FROM `sample - superstore`;
-SELECT * FROM `sample - superstore` LIMIT 5;
-RENAME TABLE `sample - superstore` TO sample_superstore;
-SELECT COUNT(*) FROM sample_superstore;
+
